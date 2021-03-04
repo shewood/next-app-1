63e943196bf4c6559c305b9841009f177507e64f
shewood <shewood01@gmail.com>
2021-03-04 18:23:29 -0500

    added image

--- components/CardItem.js
+++ components/CardItem.js
@@ -1,18 +1,49 @@
 
 import { format, parseISO } from "date-fns";
 import Link from "next/link";
+import Image from 'next/image'
 
 export function CardItem({ slug, title, date, content}) {
     return (
         //key={item.slug}
-        <div className='border border-gray-300 rounded-lg shadow-md p-4 w-4/5 bg-white'>
-        <div>
+
+<div className=' flex flex-row border bg-white rounded-lg shadow-md p-4 w-4/5 relative'>
+ 
+  <Image
+
+src="/i-like-food.svg"
+alt="food"
+// layout="fill"
+width={100}
+height={100}
+/>
+
+
+
+ 
+
+
+
+<div className=' border-gray-100 shadow-md p-4 w-4/5 bg-white absolute inset-y-0 right-0'>
+        <div >
+
           <Link href={`/blog/${slug}`}>
             <a className="font-bold text-blue-700 hover:text-blue-400;">{title}</a>
           </Link>
         </div>
         <div className='text-xs text-gray-500'>{format(parseISO(date), 'MMMM do, uuu')}</div>
         <div>{content}</div>
-      </div>
+     
+        </div>
+
+</div>
+
+
+
+
+
+
+
+   
     );
 }
